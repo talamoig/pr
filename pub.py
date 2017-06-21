d268f7b0ed51462c97463fae377fa373b1604ea7
talamoig <ivano.talamo@gmail.com>
2017-06-21 18:14:40 +0200

    Code refactoring and change of configuration file infos

--- pub.py
+++ pub.py
@@ -6,7 +6,6 @@ import os
 import re
 import sys
 import pytumblr
-import oauth2 as oauth
 import argparse
 import ConfigParser
 from subprocess import call
@@ -14,100 +13,132 @@ from subprocess import call
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-def tumblr_pub(config, filename, title, description, tags):
-    blogname=config.get('tumblr','blogname')
-    print ("[tumblr] Publishing %s on %s"%(filename, blogname))
-
+def tumblr_pub(config, account, filename, title, description, tags):
+    allowed_state = ['published', 'draft', 'queue', 'private']
+    blogname = config.get(account,'blogname')
+    state = config.get(account,'state')
+    if state not in allowed_state:
+        return "state %s not in %s"%(state, ", ".join(allowed_state))
     client = pytumblr.TumblrRestClient(
-        config.get('tumblr','consumer_key'),
-        config.get('tumblr','consumer_secret'),
-        config.get('tumblr','token'),
-        config.get('tumblr','token_secret')
+        config.get(account, 'consumer_key'),
+        config.get(account, 'consumer_secret'),
+        config.get(account, 'token'),
+        config.get(account, 'token_secret')
     )
-    result=client.create_photo(blogname, caption=title, state='draft', tags=tags, tweet=description, data=filename)
-    print(result)
-    
-CONFIGFILE='pr.ini'
-    
-config = ConfigParser.RawConfigParser()
-if not os.path.isfile(CONFIGFILE):
-    eprint('Cannot find config file %s'%CONFIGFILE)
-    sys.exit(1)
-config.read(CONFIGFILE)
+    result=client.create_photo(blogname, caption=title, state=state, tags=tags, tweet=description, data=filename)
+    if result.has_key('id'):
+        return "ok"
+    return result
 
-parser = argparse.ArgumentParser(description='Publish stuff in places')
+def conf_configfile(configfile):
+    config = ConfigParser.RawConfigParser()
+    if configfile.startswith('~'):
+        configfile=os.path.expanduser('~')+configfile[1:]
+    if not os.path.isfile(configfile):
+        eprint('Cannot find config file %s'%configfile)
+        sys.exit(1)
+    config.read(configfile)
+    return config
 
-parser.add_argument('--what',
-                    help='image to publish',
-                    required=True,
-)
-parser.add_argument('--where',
-                    help='destination platform',
-                    required=True
-)
-parser.add_argument('--title',
-                    help='title to publish',
-                    default='',
-                    required=True
-)
-parser.add_argument('--tags',
-                    required=True
-)
-parser.add_argument('--description',
-                    default='',
-                    required=True
-)
 
-args = parser.parse_args()
+def parseargs():
 
-image=args.what
-platforms=args.where
-title=args.title
-tags=args.tags.split(',')
-description=args.description
+    parser = argparse.ArgumentParser(description='Publish stuff in places')
 
-if not os.path.isfile(image):
-    eprint("File %s not found"%image)
-    sys.exit(1)
+    parser.add_argument('--what',
+                        help='image to publish',
+                        required=True,
+    )
+    parser.add_argument('--where',
+                        help='destination accounts',
+                        required=True
+    )
+    parser.add_argument('--title',
+                        help='title to publish',
+                        default='',
+                        required=True
+    )
+    parser.add_argument('--tags',
+                        required=True
+    )
+    parser.add_argument('--description',
+                        default='',
+                        required=True
+    )
 
-im=Image.open(image)
-width,height=(im.size)
+    parser.add_argument('--config',
+                        default='~/pr.ini',
+                        required=False
+    )
 
-all_platforms=config.sections()
-all_platforms.remove('general')
+    args = parser.parse_args()
+    config_file = args.config
+    image = args.what
+    accounts = args.where
+    title = args.title
+    tags = args.tags.split(',')
+    description = args.description
+    config=conf_configfile(config_file)
 
-if platforms=='all':
-    platforms=all_platforms
-else:
-    platforms=platforms.split(',')
-    for p in platforms:
-        if p not in all_platforms:
-            eprint("No section for platform %s found in the config file %s"%(p,CONFIGFILE))
-            sys.exit(1)
+    return (config,
+            image,
+            accounts,
+            title,
+            description,
+            tags)
 
-base_filename=os.path.splitext(image)[0]
-convert_cmd=config.get('general','convert')
+def main():
+    (config, image, accounts, title, description, tags) = parseargs()
 
-for p in platforms:
-    try:
-        resize_to=config.get(p,'resize_to')
-        resize_cmd='-resize %s'%resize_to
-    except e:
-        resize_cmd=''
-    try:
-        out_type=config.get(p,'out_type')
-    except e:
-        eprint('out_type not defined for %s'%p)
+    if not os.path.isfile(image):
+        eprint("File %s not found"%image)
         sys.exit(1)
 
-    destination_filename='%s_%s.%s'%(base_filename,p,out_type)
-    command="%s %s %s %s"%(convert_cmd,image,resize_cmd,destination_filename)
-    command_list=command.split(' ')
-    call(command_list)
-    method_name='%s_pub'%p
-    possibles = globals().copy()
-    possibles.update(locals())
-    method = possibles.get(method_name)
-    if not method:
-        raise NotImplementedError("Method %s not implemented" % method_name)
-    method(config, destination_filename, title, description, tags)    
+    im=Image.open(image)
+    width,height=(im.size)
+
+    all_accounts=config.sections()
+    all_accounts.remove('general')
+
+    if accounts=='all':
+        account=all_accounts
+    else:
+        accounts=accounts.split(',')
+
+    for a in accounts:
+        if a not in all_accounts:
+            eprint("No section for account %s found in the config file"%a)
+            sys.exit(1)
+
+    base_filename=os.path.splitext(image)[0]
+    convert_cmd=config.get('general','convert')
+
+    for a in accounts:
+        try:
+            resize_to=config.get(a,'resize_to')
+            resize_cmd='-resize %s'%resize_to
+        except e:
+            resize_cmd=''
+        try:
+            out_type=config.get(a,'out_type')
+        except e:
+            eprint('out_type not defined for %s'%a)
+            sys.exit(1)
+
+        destination_filename='%s_%s.%s'%(base_filename,a,out_type)
+        command="%s %s %s %s"%(convert_cmd,image,resize_cmd,destination_filename)
+        command_list=command.split(' ')
+        call(command_list)
+        platform=config.get(a,'platform')
+        method_name='%s_pub'%platform
+        possibles = globals().copy()
+        possibles.update(locals())
+        method = possibles.get(method_name)
+        if not method:
+            raise NotImplementedError("Method %s not implemented" % method_name)
+
+        result=method(config, a, destination_filename, title, description, tags)
+        print("%s: %s"%(a,result))
+
+if __name__ == "__main__":
+    main()
